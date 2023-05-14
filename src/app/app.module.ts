2917b832f87d9565b80902b351f98ad174ce962a
devszark <devszark@gmail.com>
2023-05-14 20:41:48 +0200

    Angular material added and top bar from material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ButtonsComponent } from './buttons/buttons.component';
 import { ListComponent } from './expenses/expenses-list/list.component';
 import { AddExpenseComponent } from './expenses/add-expense/add-expense.component';
 import { ReportsComponent } from './reports/reports.component';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonsComponent,
     ListComponent,
     AddExpenseComponent,
     ReportsComponent
@@ -20,7 +21,9 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
